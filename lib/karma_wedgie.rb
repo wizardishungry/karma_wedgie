079cb591c6f9ba2dc9817ecd47f791048264cf05
Jon Williams <jonathan.williams@gmail.com>
2010-12-22 00:11:30 -0500

    print out everything for debugging dude/

--- lib/karma_wedgie.rb
+++ lib/karma_wedgie.rb
@@ -1,11 +1,35 @@
 require 'rubygems'
 require 'keychain'
-require 'readline'
+
 
 class KarmaWedgie
   VERSION = '0.0.0'
-  def self.query(line)
-      item = Keychain.items.find { |item| item.password =~ Regexp.new(line) }
-      puts "#{item.account}@#{item.label}\n"
+  def query(line)
+      begin
+        item = Keychain.items.find { |item| item.password =~ Regexp.new(line) && i.security_domain }
+      rescue
+        puts "got error #{$!}\n"
+        item = nil
+      end
+
+      if item
+        puts "#{item.account}/#{item.label}\n"
+      end
+      # modified_date
+      # server
+      # authentication_type => form
+      # password
+      # account
+      # creation_date
+      # protocol => http
+      # creator
+      # path
+  end
+  def list
+    count = 0
+    Keychain.items.each do |i|
+      puts "#{i.account}\t#{i.server}\t#{i.modified_date}\t#{i.protocol}\t#{i.path}\n" 
+      count=count+1
+    end
   end
 end
