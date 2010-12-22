ebbfe2c2063ae1aba63dd5a2e01770a3dd2f30e2
Jon Williams <jonathan.williams@gmail.com>
2010-12-22 01:20:40 -0500

    better handling of terminal conditions eof, interrupt

--- lib/karma_wedgie_console.rb
+++ lib/karma_wedgie_console.rb
@@ -4,8 +4,18 @@ require 'highline/import'
 
 class KarmaWedgieConsole < KarmaWedgie
   def run
-    while line = ask("Password regexp?: "){|q| q.echo = false}
-      query(line){|range| ask "action?: " }
+    begin
+      while line = ask("Password regexp?: "){|q| q.echo = false}
+        query(line){|range| ask "action?: " }
+      end
+      rescue EOFError
+        # do nothing for now
+      rescue Interrupt
+          puts
+          system "stty echo"
+          exit
+      rescue
+        puts "ERROR: #{$!}\n"
+      end
     end
-  end
 end
