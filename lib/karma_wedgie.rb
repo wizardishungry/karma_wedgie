fe36ac88c376c3fa336ba5017ac83be9267d8fa1
Jon Williams <jonathan.williams@gmail.com>
2010-12-22 01:03:17 -0500

    some progress

--- lib/karma_wedgie.rb
+++ lib/karma_wedgie.rb
@@ -6,30 +6,43 @@ class KarmaWedgie
   VERSION = '0.0.0'
   def query(line)
       begin
-        item = Keychain.items.find { |item| item.password =~ Regexp.new(line) && i.security_domain }
+        item = @items.find { |item| item.password =~ Regexp.new(line) }
       rescue
         puts "got error #{$!}\n"
         item = nil
       end
 
       if item
-        puts "#{item.account}/#{item.label}\n"
+        display item
+        choice = yield 5
       end
-      # modified_date
-      # server
-      # authentication_type => form
-      # password
-      # account
-      # creation_date
-      # protocol => http
-      # creator
-      # path
+ 
+  end
+  def display(i)
+        puts "#{i.account}\t#{i.server}\t#{i.modified_date}\t#{i.protocol}\t#{i.path}\t#{i.creator}\n" 
   end
   def list
-    count = 0
-    Keychain.items.each do |i|
-      puts "#{i.account}\t#{i.server}\t#{i.modified_date}\t#{i.protocol}\t#{i.path}\n" 
-      count=count+1
-    end
+    @items.select { |i| 
+        #!(i.protocol=~/http|htps/) &&
+        i.protocol==nil #&& i.creator!=''
+      }.each do |i|
+        puts "#{i.account}\t#{i.server}\t#{i.modified_date}\t#{i.protocol}\t#{i.path}\t#{i.creator}\n" 
+      end
+    # FIELDS
+    # modified_date
+    # server
+    # authentication_type => form
+    # password
+    # account
+    # creation_date
+    # protocol => http,htps,afp,smtp,imap,etc <-- 4 letter codes
+    # creator
+    # path
+    # security_domain
+  end
+  def initialize
+    @items = Keychain.items.
+      select { |i| (i.protocol=~/^(http|htps)$/) }. # change this to suit you
+      sort { |a,b| b.modified_date <=> a.modified_date } # newest first
   end
 end
