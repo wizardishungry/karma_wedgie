d971161e60e566fdad07bc260971a4a40db52243
Jon Williams <jonathan.williams@gmail.com>
2010-12-21 22:41:33 -0500

    sensible sow setup

---
+++ lib/karma_wedgie.rb
@@ -0,0 +1,3 @@
+class KarmaWedgie
+  VERSION = '1.0.0'
+end
---
+++ test/test_karma_wedgie.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require "karma_wedgie"
+
+class TestKarmaWedgie < Test::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
